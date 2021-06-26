76b60b4442ad09da9ec8bb072d34858936ea65cc
Kyler <harri828@purdue.edu>
2021-06-26 17:36:43 -0400

    starting outline of the hardcoded game/player classes

---
+++ game.py
@@ -0,0 +1,15 @@
+class Game:
+
+    def __init__():
+        # TODO define deck of cards [0..107] (track human name, tuples or dict), shuffle
+        pass
+
+    def deal():
+        # TODO deal out initial cards randomly from deck
+        # should take in player object to update?
+        pass
+
+    def draw():
+        # TODO pick top card from deck, track global game card counter (reinitialize when hits num cards)
+        # should take in player object to update?
+        pass
---
+++ player.py
@@ -0,0 +1,14 @@
+class Player:
+
+    def __init__():
+        self.cards = []
+
+    def play():
+        # TODO define simple strategy from what.txt, i.e. action player should take
+        # should take in current top card from deck only bc this the strat player?
+        # or maybe pass in everything that the learning agent would see, so that
+        # this could be used for both? eh maybe not bc learning agent has more 
+        # stuff to write 
+
+        # basically, need funs to update Player state and to take action based on state 
+        pass
