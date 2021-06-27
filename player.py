776ac1aa1ffb6e31a1ba615ed86b587fe9de0d21
Kyler <harri828@purdue.edu>
2021-06-27 13:25:37 -0400

    doing inheritance from this now

--- player.py
+++ player.py
@@ -1,14 +1,22 @@
 class Player:
 
-    def __init__():
+    def __init__(self):
         self.cards = []
 
-    def play():
-        # TODO define simple strategy from what.txt, i.e. action player should take
-        # should take in current top card from deck only bc this the strat player?
-        # or maybe pass in everything that the learning agent would see, so that
-        # this could be used for both? eh maybe not bc learning agent has more 
-        # stuff to write 
+    def play_card(self, hand_idx, game):
+        """
+        hand_idx: index of self.cards to remove, not actual card index
+        game: Game object from game.py
+        """
 
-        # basically, need funs to update Player state and to take action based on state 
+        # TODO call method in game for card play 
         pass
+
+    def draw_card(self, game):
+        """
+        game: Game object from game.py
+        """
+
+        # TODO call method in game for card draw
+        pass
+
