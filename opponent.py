543d22f6ad90b0cd671d1d1d7d7a60a1859aefc4
Kyler <harri828@purdue.edu>
2021-06-27 13:26:45 -0400

    was player.py before, will now inherit from player.py and use my hardcoded strategy

---
+++ opponent.py
@@ -0,0 +1,14 @@
+class Opponent(Player):
+
+    def __init__():
+        super().__init()
+
+    def decision(self, top_card):
+        """
+        implements opponent strategy defined in what.txt, calls play_card()
+        top_card: the top card on the play deck (defined in game)
+        """
+
+        # TODO define simple strategy from what.txt, i.e. action opponent should take
+        # should take in current top card from deck only bc this the strat player?
+        pass
