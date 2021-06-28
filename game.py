a706f34c3bea9038978be15f16413d404a2e3b72
Kyler <harri828@purdue.edu>
2021-06-28 10:00:55 -0400

    draw and deal funs

--- game.py
+++ game.py
@@ -5,8 +5,10 @@ class Game:
     def __init__(self):
         self.deck = []  # deck of cards that is shuffled and removed from 
         self.play_deck = []  # deck of cards that have been played and added to, in order, will be in agent's state
+        self.cards_drawn = 0
+        self.size_deck = 108  # 108 total cards (see below), will be indexed 0..107
 
-        # card: (idx int, number or special value str, color str)
+        # card format: (idx int, number or special value str, color str)
 
         # special cards
         num_wild = 4
@@ -63,18 +65,25 @@ class Game:
         # doing it this way so that self.deck.pop() removes card tuple from deck and returns it
         # and can just check len(deck) each time to see if you need to shuffle a new deck
 
+    def draw(self):
+        """ draw top card (i.e. self.deck[-1]) from deck and return card tuple """
+        card_drawn = self.deck.pop()
+        self.cards_drawn += 1
+
+        # reshuffle deck if cards have been played (TODO make sure agent has a separate list tracking all cards that won't reset)
+        if self.cards_drawn == self.deck_size:
+            self.__init__()
+
+        return card_drawn
 
     def deal(self, players, num):
         """ 
-        players is list of player objects (Opponent and Agent only, but game can handle more)
+        players is list of player objects (Opponent and Agent only, but game can handle more 
+        - well as long as len(players) * cards_per doesn't exceed 108)
         """
-        # TODO deal out initial cards randomly from deck
-        # should take in player object to update?
-        pass
+        cards_per = 7
 
-    def draw(self):
-        # TODO pick top card from deck, track global game card counter (reinitialize when hits num cards)
-        # should take in player object to update?
+        for player in players:
+            card = self.draw()  # cards already randomized, so can just pop from deck
+            player.add_card(card)
 
-        # TODO check length after every draw, may need a reshuffle
-        pass
