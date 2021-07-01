579b274393268e92b130d23b662bb6bc750cdd3a
Kyler <harri828@purdue.edu>
2021-07-01 13:19:49 -0400

    comments

--- agent.py
+++ agent.py
@@ -2,6 +2,14 @@ class Agent(Player):
 
     def __init__(self):
         super().__init__()
+        # state: 
+        # [num_red_0, num_red_1, ..., num_blue_reverse, num_wild_red, num_wild_yellow, ..., num_wild_draw_4_blue, play_deck[0], ..., play_deck[n]]
+        # if Agent has any wilds, it will have one of each wild color in its hand (since all are possible states)
+        # each neuron/action idx corresponds to same card idx defined in game
+
+        # action space:
+        # [play_red_0, play_red_1, ..., play_blue_reverse, play_wild_red, play_wild_yellow, ..., play_wild_draw_4_blue]
+        # each neuron/action idx corresponds to same card idx defined in game
 
     def decision(self):
         """
