5e48562482038a399fb3f56570309d2c08a4beec
Kyler <harri828@purdue.edu>
2021-07-01 13:21:15 -0400

    card indexes changed - idx of blue 0 is now same for both cards, wilds handled differently

--- game.py
+++ game.py
@@ -11,8 +11,6 @@ class Game:
         if not reshuffle: 
             self.play_deck = []  
 
-        self.cards_remain = 108  # countdown when a card is drawn, reshuffle new deck when 0
-
         # card format: (idx int, number or special value str, color str)
 
         # special cards
@@ -33,42 +31,50 @@ class Game:
         uni_idx = 0
 
         # wild cards
-        self.deck += [(idx, "wild", None) for idx in range(num_wild)]  
-        uni_idx += num_wild
+        self.deck += [(uni_idx, "wild", None) for _ in range(num_wild)]  
+        uni_idx += 1
 
         # wild draw 4 cards
-        self.deck += [(idx, "wild_draw_4", None) for idx in range(uni_idx, uni_idx + num_wild_draw_4)]  
-        uni_idx += num_wild_draw_4
+        self.deck += [(uni_idx, "wild_draw_4", None) for _ in range(uni_idx, uni_idx + num_wild_draw_4)]  
+        uni_idx += 1 
 
         # color cards
         for color in colors:
             # zero
-            self.deck += [(idx, "0", color) for idx in range(uni_idx, uni_idx + num_zero)]
-            uni_idx += num_zero
+            self.deck += [(uni_idx, "0", color) for _ in range(uni_idx, uni_idx + num_zero)]
+            uni_idx += 1
 
             # numbers
             for num in range(num_min, num_max):
                 for num_count in range(num_nums):
                     self.deck.append((uni_idx, str(num), color))
-                    uni_idx += 1
+                uni_idx += 1
 
             # draw 2
-            self.deck += [(idx, "draw_2", color) for idx in range(uni_idx, uni_idx + num_draw_2)]
-            uni_idx += num_draw_2
+            self.deck += [(uni_idx, "draw_2", color) for _ in range(uni_idx, uni_idx + num_draw_2)]
+            uni_idx += 1 
 
             # reverse
-            self.deck += [(idx, "reverse", color) for idx in range(uni_idx, uni_idx + num_reverse)]
-            uni_idx += num_reverse
+            self.deck += [(uni_idx, "reverse", color) for _ in range(uni_idx, uni_idx + num_reverse)]
+            uni_idx += 1
 
             # skip
-            self.deck += [(idx, "skip", color) for idx in range(uni_idx, uni_idx + num_skip)]
-            uni_idx += num_skip
+            self.deck += [(uni_idx, "skip", color) for _ in range(uni_idx, uni_idx + num_skip)]
+            uni_idx += 1
 
         random.shuffle(self.deck)  # shuffle deck
+        self.cards_remain = len(self.deck)  # countdown when a card is drawn, reshuffle new deck when 0
+
+        # used to define a wild card index once Opponent has decided on color 
+        wild_set = [("wild", "red"), ("wild", "yellow"), ("wild", "green"), ("wild", "blue")]
+        wild_set += [(f"{elem[0]}_draw_4", elem[1]) for elem in wild_set]
+        self.wild_dict = {}
+        for wild_tuple in wild_set:
+            self.wild_dict[wild_tuple] = uni_idx
+            uni_idx += 1
 
         # NOTE irl top to bottom of deck will be idx -1..0, so top card on deck is self.deck[-1]
         # doing it this way so that self.deck.pop() removes card tuple from deck and returns it
-        # and can just check len(deck) each time to see if you need to shuffle a new deck
 
     def draw(self):
         """ draw top card (i.e. self.deck[-1]) from deck and return card tuple """
@@ -83,8 +89,8 @@ class Game:
 
     def deal(self, players, cards_per):
         """ 
-        players is list of player objects (Opponent and Agent only, but game can handle more 
-        - well as long as len(players) * cards_per doesn't exceed 108)
+        players is list of player objects (Opponent and Agent only, but game can handle more - well
+        as long as len(players) * cards_per doesn't exceed the total number of cards (108))
         """
         for player in players:
             for card_per in range(cards_per):
