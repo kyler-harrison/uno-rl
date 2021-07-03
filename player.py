91f654ceba7163252c23858c85bdc3b93fb96133
Kyler <harri828@purdue.edu>
2021-07-03 09:36:29 -0400

    fun to remove card

--- player.py
+++ player.py
@@ -5,7 +5,15 @@ class Player:
 
     def add_card(self, card):
         """
+        called when a card is added to a players hand
         card: three element card tuple defined in game.py
         """
         self.cards.append(card)
+    
+    def remove_card(self, card):
+        """
+        called when a card is played
+        card: three element card tuple defined in game.py
+        """
+        del self.cards[self.cards.index(card)]
 
