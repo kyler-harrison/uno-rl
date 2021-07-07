5c3b4ee909607ebbb73def957e32641cabccd3da
Kyler <harri828@purdue.edu>
2021-07-06 23:58:03 -0400

    fixed wild card index issus

--- game.py
+++ game.py
@@ -58,14 +58,6 @@ class Game:
 
         self.cards_remain = len(self.deck)  # countdown when a card is drawn, reshuffle new deck when 0
 
-        # add wild cards (that are not explicitly defined in deck) to cards mapping
-        wild_set = [("wild", "red"), ("wild", "yellow"), ("wild", "green"), ("wild", "blue")]
-        wild_set += [(f"{elem[0]}_draw_4", elem[1]) for elem in wild_set]
-        temp_uni_idx = uni_idx
-        for wild_tuple in wild_set:
-            self.card_dict[temp_uni_idx] = wild_tuple
-            temp_uni_idx += 1
-
         # wild cards
         self.deck += [(uni_idx, "wild", None) for _ in range(num_wild)]  
         uni_idx += 1
@@ -76,9 +68,17 @@ class Game:
         # number of playable cards (can have multiple of each)
         self.num_unique_cards = uni_idx - 1  # unassigned wilds (last two indexes) are only placeholders, idx started at 0
 
-        # add rest of cards mapping
+        # add cards to mapping
         for card in self.deck:
             self.card_dict[card[0]] = (card[1], card[2])
+
+        # add wild cards (that are not explicitly defined in deck) to cards mapping
+        wild_set = [("wild", "red"), ("wild", "yellow"), ("wild", "green"), ("wild", "blue")]
+        wild_set += [(f"{elem[0]}_draw_4", elem[1]) for elem in wild_set]
+        uni_idx += 1
+        for wild_tuple in wild_set:
+            self.card_dict[uni_idx] = wild_tuple
+            uni_idx += 1
 
         #random.shuffle(self.deck)  # shuffle deck
 
